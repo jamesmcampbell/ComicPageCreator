59546d801f62d205635a74ec597a0d3dbe03da02
James <jamwap@pm.me>
2020-04-23 23:40:15 -0700

    Added canvas shapes

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-from tkinter import
+from tkinter import *
 
-root = Tk()
+window = Tk()
+window.configure(bg='purple')
+window.geometry('1280x720')
+window.title("Comic Page Creator")
+
+canvas = Canvas(window, height=720, width=720, bg='blue')
+canvas.create_line(0,0,720,720, width=50)
+canvas.create_rectangle(0,0,720,720, width=50)
+canvas.pack()
+
+# label = Label(canvas, text="Test Label")
+# label.configure(bg='blue', fg='white')
+# label.pack()
+
+window.mainloop()
