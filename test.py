c0e4bf74e8c2a05ca33a0fd0ff95d32f824ef27e
James <jamwap@pm.me>
2020-04-24 13:07:23 -0700

    Test update

--- test.py
+++ test.py
@@ -1,12 +1,35 @@
 from tkinter import *
 
-window = Tk()
-window.configure(bg='purple')
-window.geometry('1280x720')
-window.title("Comic Page Creator")
+root = Tk()
+root.configure(bg='black')
+root.geometry('1280x720')
+root.title("Comic Page Creator")
 
-canvas = Canvas(window, height=720, width=720, bg='blue', highlightthickness=0)
-canvas.create_rectangle(0,0,720,720, width=30)
-canvas.pack()
+canvas = Canvas(root, height=720, width=720, bg='white', highlightthickness=0)
 
-window.mainloop()
+click_num = 0
+
+# This covers both rectangles as well as polygons. The reason they are in the same method is to make it easier switch between the two
+def draw_shape(click):
+	global x1, y1, x2, y2, click_num
+	x1 = y1 = x2 = y2 = 0
+
+	if click_num == 0:
+		x1 = click.x
+		y1 = click.y
+		click_num += 1
+	else:
+		x2 = click.x
+		y2 = click.y
+		click_num -= 1
+		canvas.create_rectangle(x1, y1, x2, y2, width=5)
+
+def main():
+	# global click_num
+	canvas.bind('<Button-1>', draw_shape)
+	# click_num = 0
+	canvas.pack()
+
+	root.mainloop()
+
+main()
