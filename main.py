6309ce12b577432c7da6c8ad1353c332a777d4aa
James <jamwap@pm.me>
2020-04-24 11:41:27 -0700

    window changed to root

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from tkinter import *
 
-window = Tk()
-window.configure(bg='purple')
-window.geometry('1280x720')
-window.title("Comic Page Creator")
+root = Tk()
+root.configure(bg='purple')
+root.geometry('1280x720')
+root.title("Comic Page Creator")
 
-canvas = Canvas(window, height=720, width=720, bg='blue')
-canvas.create_line(0,0,720,720, width=50)
-canvas.create_rectangle(0,0,720,720, width=50)
+canvas = Canvas(root, height=720, width=720, bg='blue', highlightthickness=0)
+# canvas.create_line(0,0,720,720, width=50)
+canvas.create_rectangle(0,0,720,720, width=30)
 canvas.pack()
 
 # label = Label(canvas, text="Test Label")
 # label.configure(bg='blue', fg='white')
 # label.pack()
 
-window.mainloop()
+root.mainloop()
