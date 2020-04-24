05d4d0ef68628673ac6bb5a5f5c41e81baba1f3a
James <jamwap@pm.me>
2020-04-24 13:18:06 -0700

    test update

--- test.py
+++ test.py
@@ -7,27 +7,24 @@ root.title("Comic Page Creator")
 
 canvas = Canvas(root, height=720, width=720, bg='white', highlightthickness=0)
 
-click_num = 0
+x1 = y1 = x2 = y2 = click_num = 0
 
 # This covers both rectangles as well as polygons. The reason they are in the same method is to make it easier switch between the two
-def draw_shape(click):
+def draw_shape(point):
 	global x1, y1, x2, y2, click_num
-	x1 = y1 = x2 = y2 = 0
 
 	if click_num == 0:
-		x1 = click.x
-		y1 = click.y
+		x1 = point.x
+		y1 = point.y
 		click_num += 1
 	else:
-		x2 = click.x
-		y2 = click.y
+		x2 = point.x
+		y2 = point.y
 		click_num -= 1
 		canvas.create_rectangle(x1, y1, x2, y2, width=5)
 
 def main():
-	# global click_num
 	canvas.bind('<Button-1>', draw_shape)
-	# click_num = 0
 	canvas.pack()
 
 	root.mainloop()
