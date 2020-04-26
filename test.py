3208a77c95e44dc9c04c5db7e18d65cb24c43fee
jamesmcampbell <58498673+jamesmcampbell@users.noreply.github.com>
2020-04-25 19:24:45 -0700

    Update test.py

--- test.py
+++ test.py
@@ -7,24 +7,39 @@ root.title("Comic Page Creator")
 
 canvas = Canvas(root, height=720, width=720, bg='white', highlightthickness=0)
 
-x1 = y1 = x2 = y2 = click_num = 0
+x0 = y0 = x1 = y1 = click_num = 0
 
 # This covers both rectangles as well as polygons. The reason they are in the same method is to make it easier switch between the two
 def draw_shape(point):
-	global x1, y1, x2, y2, click_num
+	global x0, y0, x1, y1, click_num
 
 	if click_num == 0:
-		x1 = point.x
-		y1 = point.y
+		x0, y0 = point.x, point.y
 		click_num += 1
 	else:
-		x2 = point.x
-		y2 = point.y
+		x1, y1 = point.x, point.y
 		click_num -= 1
-		canvas.create_rectangle(x1, y1, x2, y2, width=5)
+		canvas.create_rectangle(x0, y0, x1, y1, width=5)
+
+def draw(point):
+	if canvas.old_coords:
+		x0, y0 = canvas.old_coords
+		canvas.create_line(x0, y0, point.x, point.y)
+	canvas.old_coords = point.x, point.y
+
+def draw_cross(point):
+	if canvas.old_coords:
+		x0, y0 = canvas.old_coords
+		canvas.create_line(x0, y0, point.x, y0)
+		canvas.create_line(x0, y0, x0, point.y)
+	else:
+		canvas.old_coords = point.x, point.y
 
 def main():
-	canvas.bind('<Button-1>', draw_shape)
+	canvas.old_coords = None
+	# canvas.bind('<Button-1>', draw_shape)
+	# canvas.bind('<B1-Motion>', draw)
+	canvas.bind('<B1-Pressed>', draw_cross)
 	canvas.pack()
 
 	root.mainloop()
